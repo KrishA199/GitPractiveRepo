664d25b835922f6026b903afa0fc15ff5ac51256
student <s528751@ch140001m.local>
2017-10-04 12:04:30 -0500

    Initial Commit

---
+++ GitPractiveRepo/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  GitPractiveRepo
+//
+//  Created by Mucherla,Veera Kishon Kumar on 10/4/17.
+//  Copyright © 2017 Mucherla,Veera Kishon Kumar. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
